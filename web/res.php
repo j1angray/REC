fbf3ad64e2d8a2b88132f5d02d91a1f48dd5f231
RUIYINJIANG <55040026+j1angray@users.noreply.github.com>
2021-03-10 20:54:36 -0500

    Add files via upload

---
+++ web/res.php
@@ -0,0 +1,19 @@
+<html>
+<body style = "background-color:#dce7f3;">
+
+<h1 style = 'font-family:Arial; text-align:center; font-style:italic; font-variant: small-caps;' > Movie Recommendations based on your Rates</h1>
+
+<style> 
+th {background: black; color: white; font-weight: bold; padding: 6px; border: 1px solid grey; text-align: center;font-style:italic;} 
+td {padding: 6px; border: 1px solid black; text-align: center;} 
+table {margin-left: auto; margin-right: auto;}
+</style>
+
+<table> 
+	<tr> <th>MOVIE</th> <th>YEAR</th> <th>GENRE</th> <th>IMDB ID</th> </tr>
+	<tr> <td>PK</td> <td>2014</td> <td>Comedy, Drama, Romance</td> <td><a href="http://www.imdb.com/title/tt2338151/">2338151</a></td> </tr>
+</table>
+
+<h2 style = 'font-family:Arial; text-align:center; font-style:italic; font-variant: small-caps;'>Enjoy!</h2>
+</body>
+</html>
